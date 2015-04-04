769947702aa988e937d4b6b6c85c816e476d835e
Lukasz Kusek <lukasz@kusek.info>
2015-04-04 22:48:43 +0100

    Better error reporting

---
+++ src/main/java/com/github/lukaszkusek/roulette/rest/RouletteApplicationConfiguration.java
@@ -0,0 +1,36 @@
+package com.github.lukaszkusek.roulette.rest;
+
+import cz.jirutka.spring.exhandler.RestHandlerExceptionResolver;
+import cz.jirutka.spring.exhandler.support.HttpMessageConverterUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+
+import java.util.List;
+
+@Configuration
+public class RouletteApplicationConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(exceptionHandlerExceptionResolver());
+        resolvers.add(restExceptionResolver());
+    }
+
+    @Bean
+    public RestHandlerExceptionResolver restExceptionResolver() {
+        return RestHandlerExceptionResolver.builder()
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @Bean
+    public ExceptionHandlerExceptionResolver exceptionHandlerExceptionResolver() {
+        ExceptionHandlerExceptionResolver resolver = new ExceptionHandlerExceptionResolver();
+        resolver.setMessageConverters(HttpMessageConverterUtils.getDefaultHttpMessageConverters());
+        return resolver;
+    }
+}
